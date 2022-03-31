fc090145a58586975db446d5a30673c50b973156
Julian Perez <79658356+juperez143@users.noreply.github.com>
2022-03-30 20:43:24 -0600

    Add project files.

---
+++ DataTypes/DataTypes.cpp
@@ -0,0 +1,55 @@
+// DataTypes.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+
+namespace biggiecheese
+{
+    int i = 10;
+    
+
+    
+
+}
+
+
+
+int main()
+{
+    int i = -34;
+        float f = 23;
+        bool b = 5;
+        string name;
+
+        const float TAX = 0.085;
+        char intial;
+
+        int ages[4];
+        ages[0] = 24;
+
+
+        cin >> name;
+        cout << name;
+
+        if (i) cout << "true\n";
+
+    cout << biggiecheese::i << endl;
+    cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
